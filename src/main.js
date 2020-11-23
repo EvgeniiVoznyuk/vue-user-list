b6b0ee3bf0f1675b7805a8a9f91104b74c444f0c
Evgenii <voznukzhenia@gmail.com>
2020-11-23 15:29:39 +0200

    start develop

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import createServer from '@/mirage/server';
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+createServer();
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Toolbar v-on:add-user="addUser" v-on:set-query="setQuery" />
+    <Table :users="filtredUsers()" v-on:delete-users="deleteUsers" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+<script lang="">
+import Vue from 'vue';
+// import Toolbar from '../components/Toolbar.vue';
+// import Table from '../components/Table.vue';
 
-export default {
-  name: 'Home',
+export default Vue.extend({
+  name: 'Users',
   components: {
-    HelloWorld,
+
   },
-};
+  created() {
+    fetch('/api/users')
+      .then((res) => res.json())
+      .then((json) => {
+        this.users = json.users;
+      });
+    this.$emit('show-users');
+  },
+  data() {
+    return {
+      users: [],
+      searchQuery: '',
+    };
+  },
+  methods: {
+    addUser(user) {
+      const newUser = {
+        ...user,
+        role: user.role.name,
+        id: this.users.length + 1,
+      };
+      this.users.unshift(newUser);
+    },
+    setQuery(query) {
+      this.searchQuery = query;
+    },
+    filtredUsers() {
+      return this.users.filter(
+        ({ name, userName, email }) => name.toLowerCase()
+          .includes(this.searchQuery.toLowerCase()) || userName.toLowerCase()
+          .includes(this.searchQuery.toLowerCase()) || email.toLowerCase()
+          .includes(this.searchQuery.toLowerCase()),
+      );
+    },
+    deleteUsers(usersId) {
+      this.users = this.users.filter((user) => !usersId.includes(user.id));
+    },
+  },
+});
 </script>
