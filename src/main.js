82d980ddaa222828b00558bbe0b0234732ff4c57
Evgenii <voznukzhenia@gmail.com>
2020-11-23 16:42:53 +0200

    deploy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 import 'primevue/resources/themes/saga-blue/theme.css';
+import Button from 'primevue/button';
 import createServer from '@/mirage/server';
 import Vue from 'vue';
 import App from './App.vue';
@@ -8,6 +9,8 @@ import './registerServiceWorker';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.component('Button', Button);
 
 createServer();
 
